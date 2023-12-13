be5a69f055f5c17c24d108a10c49c47f4acf00ac
StivenRuiz2005 <carlosruizrojas2005@gmail.com>
2023-12-13 17:30:34 -0500

    Renombrando

---
+++ app/src/main/scala/Proyecto/Proyecto.scala
@@ -0,0 +1,18 @@
+/**
+  * Taller 3 - Programación Funcional
+  * Autores: <Estudiantes>
+  * Profesor: Carlos A Delgado
+  */
+package taller4
+
+import org.scalameter.measure
+import org.scalameter.withWarmer
+import org.scalameter.Warmer
+
+object Proyecto{
+
+  val alfabeto = Seq('a', 'c', 'g', 't')
+  type Oraculo = Seq[Char] => Boolean
+
+
+ }
