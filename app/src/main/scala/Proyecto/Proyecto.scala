c55da15eb01d141ab2504c27905538b681b66fcb
KarenGrijalba <karen.grijalba@correounivalle.edu.co>
2023-12-16 14:14:30 -0500

    reconstruir cadena

--- app/src/main/scala/Proyecto/Proyecto.scala
+++ app/src/main/scala/Proyecto/Proyecto.scala
@@ -42,7 +42,70 @@ object Proyecto {
     reconstruirCadena()
   }
 
+/*
+  def reconstruirCadena(o: Oraculo): Seq[Char] = {
+    val cadena = Seq('a', 'c')
+    def resultado(): Seq[Char] = {
+        if (o(cadena)) cadena
+        else {
+          println("No es igual")
+          Seq.empty[Char]
+        }
+      }
+    resultado()
+    }*/
 
+  def reconstruirCadenaMejorado(n: Int, o: Oraculo): Seq[Char] = {
+
+    def generarCadenasCandidatas(k: Int, candidatas: Seq[Seq[Char]]): Seq[Char] = {
+      //println(candidatas)
+      if (k == n) {
+        candidatas.head
+      } else {
+        val nuevasCandidatas = candidatas.flatMap { candidata =>
+          alfabeto.flatMap { elemento =>
+            if (o(candidata :+ elemento)) Seq(candidata :+ elemento)
+            else Seq.empty[Seq[Char]]
+          }
+        }
+        generarCadenasCandidatas(k + 1, nuevasCandidatas)
+      }
+    }
+
+      def reconstruirCadena(): Seq[Char] = {
+
+        def cadenasCandidatas(): Seq[Char] = {
+          val cadenas = alfabeto.map(Seq(_))
+
+          generarCadenasCandidatas(1, cadenas)
+        }
+        cadenasCandidatas()
+      }
+    reconstruirCadena()
+  }
+
+  def reconstruirCadenaTurbo(n: Int, o: Oraculo): Seq[Char] = {
+    def construirSubcadena(k: Int, subcadena: Seq[Seq[Char]]): Seq[Char] = {
+      if (k == n) subcadena.head
+      else {
+        val nuevoSubcadena = subcadena.flatMap { secuencia =>
+          subcadena.flatMap { elemento =>
+            val nuevaSecuencia = secuencia ++ elemento
+            if (o(nuevaSecuencia)) Seq(nuevaSecuencia)
+            else Seq.empty[Seq[Char]]
+          }
+        }
+        construirSubcadena(k * 2, nuevoSubcadena)
+      }
+    }
+    val cadenas = alfabeto.map(Seq(_))
+    val subcadena = construirSubcadena(1, cadenas)
+    subcadena
+  }
+
+  def reconstruirCadenaTurboMejorada(n: Int, o: Oraculo): Seq[Char] = {
+    Seq[Char]()
+  }
 }
 
 
--- app/src/test/scala/Proyecto/TestProyecto.scala
+++ app/src/test/scala/Proyecto/TestProyecto.scala
@@ -12,6 +12,7 @@ import org.scalatestplus.junit.JUnitRunner
 
 @RunWith(classOf[JUnitRunner])
 class TestProyecto extends AnyFunSuite{
+
     test("Prueba 1: Reconstruir cadena ingenuo"){
         type Oraculo = Seq[Char] => Boolean
         val n = 3
@@ -33,6 +34,86 @@ class TestProyecto extends AnyFunSuite{
         val o: Oraculo = (s: Seq[Char]) => s == Seq('a', 't', 'c', 'g', 't')
         val resultado = Proyecto.reconstruirCadenaIngenuo(n, o)
         assert(resultado == Seq('a', 't', 'c', 'g', 't'))
+    }
+    test("Prueba 4: Reconstruir cadena ingenuo") {
+        type Oraculo = Seq[Char] => Boolean
+        val n = 4
+        val o: Oraculo = (s: Seq[Char]) => s == Seq('a', 't', 'c', 'g')
+        val resultado = Proyecto.reconstruirCadenaIngenuo(n, o)
+        assert(resultado == Seq('a', 't', 'c', 'g'))
+    }
+    /*
+    test("Prueba 4: Reconstruir cadena "){
+        type Oraculo = Seq[Char] => Boolean
+
+        //val o: Oraculo = (s: Seq[Char]) => s.forall(Seq('a', 'c', 'g').contains)
+        val o: Oraculo = (s: Seq[Char]) => Seq('a', 'c', 'g').mkString.contains(s.mkString)
+        val resultado = Proyecto.reconstruirCadena(o)
+        println("Resultado: " + resultado)
+        assert(true)
+    }*/
+    test("Prueba 1: Reconstruir cadena mejorado"){
+        type Oraculo = Seq[Char] => Boolean
+        val n = 3
+        val o: Oraculo = (s: Seq[Char]) => Seq('a', 'c', 'g').mkString.contains(s.mkString)
+        val resultado = Proyecto.reconstruirCadenaMejorado(n, o)
+        assert(resultado == Seq('a', 'c', 'g'))
+    }
+    test("Prueba 2: Reconstruir cadena mejorado"){
+        type Oraculo = Seq[Char] => Boolean
+        val n = 4
+        val o: Oraculo = (s: Seq[Char]) => Seq('a', 'a', 'c', 'g').mkString.contains(s.mkString)
+        val resultado = Proyecto.reconstruirCadenaMejorado(n, o)
+        assert(resultado == Seq('a', 'a', 'c', 'g'))
+    }
+    test("Prueba 3: Reconstruir cadena mejorado"){
+        type Oraculo = Seq[Char] => Boolean
+        val n = 5
+        val o: Oraculo = (s: Seq[Char]) => Seq('a', 't', 'c', 'g', 't').mkString.contains(s.mkString)
+        val resultado = Proyecto.reconstruirCadenaMejorado(n, o)
+        assert(resultado == Seq('a', 't', 'c', 'g', 't'))
+    }
+    test("Prueba 4: Reconstruir cadena mejorado"){
+        type Oraculo = Seq[Char] => Boolean
+        val n = 5
+        val o: Oraculo = (s: Seq[Char]) => Seq('a', 'g', 'c', 'a', 't').mkString.contains(s.mkString)
+        val resultado = Proyecto.reconstruirCadenaMejorado(n, o)
+        assert(resultado == Seq('a', 'g', 'c', 'a', 't'))
+    }
+    test("Prueba 1: Reconstruir cadena turbo"){
+        type Oraculo = Seq[Char] => Boolean
+        val n = 4
+        val o: Oraculo = (s: Seq[Char]) => Seq('a', 'c', 'g', 'c').mkString.contains(s.mkString)
+        val resultado = Proyecto.reconstruirCadenaTurbo(n, o)
+        assert(resultado == Seq('a', 'c', 'g', 'c'))
+    }
+    test("Prueba 2: Reconstruir cadena turbo"){
+        type Oraculo = Seq[Char] => Boolean
+        val n = 4
+        val o: Oraculo = (s: Seq[Char]) => Seq('c', 'g', 'c', 't').mkString.contains(s.mkString)
+        val resultado = Proyecto.reconstruirCadenaTurbo(n, o)
+        assert(resultado == Seq('c', 'g', 'c', 't'))
+    }
+    test("Prueba 3: Reconstruir cadena turbo"){
+        type Oraculo = Seq[Char] => Boolean
+        val n = 4
+        val o: Oraculo = (s: Seq[Char]) => Seq('g', 'c', 't', 'a').mkString.contains(s.mkString)
+        val resultado = Proyecto.reconstruirCadenaTurbo(n, o)
+        assert(resultado == Seq('g', 'c', 't', 'a'))
+    }
+    test("Prueba 4: Reconstruir cadena turbo"){
+        type Oraculo = Seq[Char] => Boolean
+        val n = 4
+        val o: Oraculo = (s: Seq[Char]) => Seq('t', 'a', 'c', 'g').mkString.contains(s.mkString)
+        val resultado = Proyecto.reconstruirCadenaTurbo(n, o)
+        assert(resultado == Seq('t', 'a', 'c', 'g'))
+    }
+    test("Prueba 1: Reconstruir cadena turbo mejorada"){
+        type Oraculo = Seq[Char] => Boolean
+        val n = 4
+        val o: Oraculo = (s: Seq[Char]) => Seq('a', 'c', 'g', 'c').mkString.contains(s.mkString)
+        val resultado = Proyecto.reconstruirCadenaTurboMejorada(n, o)
+        assert(resultado == Seq('a', 'c', 'g', 'c'))
     }
 
 }
